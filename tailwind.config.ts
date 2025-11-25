869e4404f299d3e6d50ee7eca7166f520f0ec5f3
ujjwal6713 <ujjwalupreti2014@gmail.com>
2025-11-24 19:32:37 -0500

    Add important flag to Tailwind and force rebuild

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  important: true,
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
